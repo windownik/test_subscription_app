966e4ddfded8e952721c0cc0dacea77fffd35e8e
Nikita Mislivets <nikitamislivets@gmail.com>
2026-06-26 20:46:39 +0300

    add isActive parameter to CircularIconButton

--- lib/core/extensions/cupertino_theme_colors_extension.dart
+++ lib/core/extensions/cupertino_theme_colors_extension.dart
@@ -16,4 +16,23 @@ extension CupertinoThemeColorsExtension on BuildContext {
   }
 
   static const double circularIconButtonShadowOpacity = 0.25;
+  static const double circularIconButtonInactiveOpacity = 0.4;
+
+  Color resolveCircularIconButtonBackground({required bool isActive}) {
+    final background = circularIconButtonBackground;
+
+    if (isActive) {
+      return background;
+    }
+
+    return background.withValues(alpha: circularIconButtonInactiveOpacity);
+  }
+
+  Color resolveCircularIconButtonShadow({required bool isActive}) {
+    if (!isActive) {
+      return CupertinoColors.transparent;
+    }
+
+    return circularIconButtonShadow;
+  }
 }
--- lib/core/presentation/widgets/circular_icon_button.dart
+++ lib/core/presentation/widgets/circular_icon_button.dart
@@ -12,6 +12,7 @@ class CircularIconButton extends StatefulWidget {
   final Widget child;
   final double size;
   final double padding;
+  final bool isActive;
   final VoidCallback onTap;
 
   const CircularIconButton({
@@ -20,6 +21,7 @@ class CircularIconButton extends StatefulWidget {
     required this.onTap,
     this.size = defaultSize,
     this.padding = defaultPadding,
+    this.isActive = true,
   });
 
   @override
@@ -31,25 +33,30 @@ class _CircularIconButtonState extends State<CircularIconButton> {
 
   @override
   Widget build(BuildContext context) {
-    final scale =
-        _isPressed ? CircularIconButton.pressedScale : 1.0;
+    final scale = widget.isActive && _isPressed
+        ? CircularIconButton.pressedScale
+        : 1.0;
 
     return GestureDetector(
-      onTap: widget.onTap,
-      onTapDown: (_) => onTapDown(),
-      onTapUp: (_) => onTapUp(),
-      onTapCancel: onTapCancel,
+      onTap: widget.isActive ? widget.onTap : null,
+      onTapDown: widget.isActive ? (_) => onTapDown() : null,
+      onTapUp: widget.isActive ? (_) => onTapUp() : null,
+      onTapCancel: widget.isActive ? onTapCancel : null,
       child: AnimatedContainer(
         duration: CircularIconButton.pressAnimationDuration,
         curve: Curves.easeInOut,
         width: widget.size * scale,
         height: widget.size * scale,
         decoration: BoxDecoration(
-          color: context.circularIconButtonBackground,
+          color: context.resolveCircularIconButtonBackground(
+            isActive: widget.isActive,
+          ),
           shape: BoxShape.circle,
           boxShadow: [
             BoxShadow(
-              color: context.circularIconButtonShadow,
+              color: context.resolveCircularIconButtonShadow(
+                isActive: widget.isActive,
+              ),
               blurRadius: CircularIconButton.shadowBlurRadius,
               offset: const Offset(
                 0,
